cd3e0e4e48d6baaa735d80fb274755cd3088c212
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-19 15:45:27 -0700

    added GET api method

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
+require("dotenv").config;
 const express = require("express");
 
 const app = express();
 
-const port = 3005;
+// GET all Restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "succes",
+        data: {
+            restaurant: ["Zubu", "Wendys"]
+        }
+    });
+});
+
+const port = process.env.PORT || 3005;
 app.listen(port, () => {
     console.log(`server is up and listening on port ${port}`);
 });
