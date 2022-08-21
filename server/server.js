955ac1f3e1e0923bdc1a9cc5dc940b6bfca174a8
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-20 22:10:01 -0700

    added middleware and added CRUD APIs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 require("dotenv").config;
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+
+app.use(express.json());
+
+// app.use(morgan("dev"));
+
+// Middleware define at the top
+// app.use((req, res, next) => {
+//     console.log("our middleware");
+//     next(); // Jump to next middleware
+// });
 
 // GET all Restaurants
 app.get("/api/v1/restaurants", (req, res) => {
@@ -15,11 +25,26 @@ app.get("/api/v1/restaurants", (req, res) => {
 
 // GET a Restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req);
+    console.log(req.params);
+
+    res.status(200).json({
+        
+    })
 });
 
 //CREATE a restaurant
 app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body);
+});
+
+// UPDATE a restaurant
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+});
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
     
 })
 
