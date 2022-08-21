a0b390a009da0b39c34b956a7d8a423de90d69a1
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-20 22:45:59 -0700

    Update gitignore

---
+++ server/DB/db.sql
@@ -0,0 +1,28 @@
+-- for help:  command \?
+
+-- for list of database: command \l
+
+-- CREATE DATABASE 'name of the db';
+
+-- for connect to database: \c 'name of the db'
+
+-- for list table: \d 
+
+CREATE TABLE products (
+    id INT, 
+    name VARCHAR(50),
+    price INT,
+    on_sale boolean
+);
+
+ALTER TABLE products ADD COLUMN featured boolean;
+ALTER TABLE products DROP COLUMN featured;
+
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (id, name, location, price_range) values (123, 'zubu', 'Burnaby', 4);
