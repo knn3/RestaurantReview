8e1c2b3dac4c019ac38974adf6db983054e27598
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-22 13:15:14 -0700

    Added components and design Home page

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import AddRestaurant from '../components/AddRestaurant'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+      <div>
+          <Header />
+          <AddRestaurant/>
+    </div>
   )
 }
 
