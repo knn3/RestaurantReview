5b9257369b8be0153e57b4bc57a7a20ea3118b01
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-23 22:14:38 -0700

    Rendered data from database

--- client/src/components/RestaurantLists.jsx
+++ client/src/components/RestaurantLists.jsx
@@ -30,8 +30,24 @@ const RestaurantLists = (props) => {
             <th scope="col">Delete</th>
           </tr>
         </thead>
-        <tbody className="table-dark">
-          <tr>
+              <tbody className="table-dark">
+                  {restaurants && restaurants.map(restaurant => {
+                      return (
+                        <tr key={restaurant.id}>
+                          <td>{restaurant.name}</td>
+                          <td>{restaurant.location}</td>
+                          <td>{"$".repeat(restaurant.price_range)}</td>
+                          <td>reviews</td>
+                          <td>
+                            <button className="btn btn-warning">Edit</button>
+                          </td>
+                          <td>
+                            <button className="btn btn-danger">Delete</button>
+                          </td>
+                        </tr>
+                      ); 
+                  })}
+          {/* <tr>
             <td>McDonalds</td>
             <td>New York</td>
             <td>$$$$</td>
@@ -54,7 +70,7 @@ const RestaurantLists = (props) => {
             <td>
               <button className="btn btn-danger">Delete</button>
             </td>
-          </tr> 
+          </tr>  */}
         </tbody>
       </table>
     </div>
