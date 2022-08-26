c46c093c53832504fe974c34b330b58c7a80c808
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-26 14:05:16 -0700

    Update reviews database table

--- server/DB/db.sql
+++ server/DB/db.sql
@@ -26,3 +26,11 @@ CREATE TABLE restaurants (
 );
 
 INSERT INTO restaurants (id, name, location, price_range) values (123, 'zubu', 'Burnaby', 4);
+
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL CHECK(rating >= 1 AND rating <= 5)
+);
