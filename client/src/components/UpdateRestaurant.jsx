78420afeec2ae2e725318c8dfa91a4ce448687d0
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-26 10:09:17 -0700

    Finished update restaurant form

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -4,10 +4,33 @@ import { useParams } from 'react-router-dom'
 const UpdateRestaurant = (props) => {
     const { id } = useParams();
   return (
-      <div>
-          
+    <div className="container">
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input id="name" className="form-control" type="text" />
+        </div>
+        
+        <br />
+        
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input id="location" className="form-control" type="text" />
+        </div>
+        
+        <br />
+
+        <div className="form-group">
+          <label htmlFor="price_range">Price Range</label>
+          <input id="price_range" className="form-control" type="number" />
+        </div>
+        
+        <br/>
+        
+        <button className='btn btn-primary'>Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default UpdateRestaurant
